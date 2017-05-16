339103e0d9a79e73790c7c62f20926c9f7370287
jsemple19 <semple.j@gmail.com>
2017-05-16 11:55:08 +0200

    dealing with cases where input type is not included

--- convertingGeneNamesFunction1.R
+++ convertingGeneNamesFunction1.R
@@ -1,35 +1,61 @@
 #2017-05-08 using table downloaded from wormMine
-#script to convert c.elegans gene names in between wb, cosmid and public gene names 
+#script to convert c.elegans gene names in between wb, cosmid and public gene names
 #setwd("/home/jenny/Documents/MeisterLab/GenomeVer/geneNameConversion")
 
-#downloaded from Worm Mine. using Query builder. Choosing attributes
-#Gene>sequence Name
-#Gene>WormBase Gene ID
-#Gene> Gene name
+# #downloaded from Worm Mine. using Query builder. Choosing attributes
+# #Gene>sequence Name
+# #Gene>WormBase Gene ID
+# #Gene> Gene name
+# # do once to pre-process table
+# geneNameFile<-"~/Documents/MeisterLab/GenomeVer/geneNameConversion/20170514_wormMine_geneNameAttributes.tsv"
+# GeneNameTable<-read.delim(geneNameFile, stringsAsFactors=FALSE,header=FALSE)
+# names(GeneNameTable)<-c("WBgeneID","seqID","publicID")
+# #remove strange duplicate names in the beginning
+# GeneNameTable<-GeneNameTable[-grep("Gene:WBGene",GeneNameTable$WBgeneID),]
+# #complete empty names in seqID and publicID one wiht the other
+# GeneNameTable$seqID<-ifelse((GeneNameTable$seqID==""),GeneNameTable$publicID,GeneNameTable$seqID)
+# GeneNameTable$publicID<-ifelse((GeneNameTable$publicID==""),GeneNameTable$seqID,GeneNameTable$publicID)
+# sum(duplicated(GeneNameTable$publicID)) #46
+# sum(duplicated(GeneNameTable$seqID)) #49
+# sum(duplicated(GeneNameTable$WBgeneID)) #0
+# #need to handle duplicates problem for proper matching. to it within funciton according to
+# #input type
+#
+# write.table(GeneNameTable,"~/Documents/MeisterLab/GenomeVer/geneNameConversion/20170514_wormMine_geneNameAttributes.csv",
+#             quote=FALSE,row.names=FALSE,sep=",")
 
-# do once
-# GeneNameTable<-read.table("/home/jenny/Documents/MeisterLab/GenomeVer/geneNameConversion/wormbaseNameConversion.tsv",stringsAsFactors=FALSE)
-# names(GeneNameTable)<-c("seqID","WBgeneID","geneName")
-# GeneNameTable<-GeneNameTable[GeneNameTable$seqID!="",]
-# temp<-GeneNameTable$geneName
-# GeneNameTable$geneName<-GeneNameTable$seqID
-# i<-which(temp!="")
-# GeneNameTable$geneName[i]<-temp[i]
-# write.csv(GeneNameTable,"/home/jenny/Documents/MeisterLab/GenomeVer/geneNameConversion/wormbaseNameConversion.csv",
-#           row.names=FALSE)
-
-#GeneNameTable<-read.csv("/home/jenny/Documents/MeisterLab/GenomeVer/geneNameConversion/wormbaseNameConversion.csv",stringsAsFactors=FALSE)
 
 convertGeneNames<-function(inputList,inputType="seqID",
-                           outputType=c("seqID","WBgeneID","geneName")){
-  GeneNameTable<-read.csv("/home/jenny/Documents/MeisterLab/GenomeVer/geneNameConversion/wormbaseNameConversion.csv",
-                            stringsAsFactors=FALSE)
+                           outputType=c("seqID","WBgeneID","publicID")){
+  geneNameFile<-"~/Documents/MeisterLab/GenomeVer/geneNameConversion/20170514_wormMine_geneNameAttributes.csv"
+  GeneNameTable<-read.csv(geneNameFile, stringsAsFactors=FALSE,header=TRUE)
 
-  i<-match(GeneNameTable[,inputType],inputList)
-  output<-GeneNameTable[i,outputType]
-  
-  ## need to deal with duplicates.. still needs work
-  #convertedIDs<-conversionTable$wormbase_gene[match(as.vector(inputList[,1]),conversionTable[,inputType])]
+  #to make matching work, first remove duplicated genes for inputType from table
+  GeneNameTable<-GeneNameTable[!duplicated(GeneNameTable[,inputType]),]
+
+  #find genes  in the input list that have no matches
+  i<-match(inputList,GeneNameTable[,inputType])
+  nonMatching<-inputList[is.na(i)]
+
+  #find genes that do match
+  i<-na.omit(i)
+  output<-GeneNameTable[i,c(inputType,outputType)]
+
+  #now add back the nonMatching ones with NA values
+  output_nm<-as.data.frame(matrix(nrow=length(nonMatching),ncol=dim(output)[2]),stringsAsFactors=FALSE)
+  output_nm[,1]<-nonMatching
+  names(output_nm)<-c(inputType,outputType)
+  output<-rbind(output,output_nm)
+
+  #reorder output table to match input list table
+  i<-match(inputList,output[,1])
+  output<-output[i,]
+  #remove column of duplicated inputType if present
+  if (inputType %in% outputType) {
+    output<-output[,-grep(".1",names(output))]
+  } else {
+    output<-output[,-grep(inputType,names(output))]
+  }
   return(output)
 }
 
